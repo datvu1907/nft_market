e3b842f2096f8e1c43781119cae885f6267c3189
VuNVD <vunvd@bap.jp>
2022-06-08 17:32:22 +0700

    get list sell

--- contract/ExchangeNFT.sol
+++ contract/ExchangeNFT.sol
@@ -11,6 +11,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 // import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "hardhat/console.sol";
 
 contract ExchangeNFT is Ownable {
     address public BoxNFT;
@@ -41,12 +42,22 @@ contract ExchangeNFT is Ownable {
         uint256 price;
         string name;
         Status status;
-        Rarity Rarity;
+        Rarity rarity;
         uint256 rarityNumber;
         BoxType boxType;
     }
 
-    mapping(uint256 => BoxExchange) listBoxExchange;
+    struct BoxItem {
+        uint256 id;
+        address owner;
+        uint256 price;
+        Status status;
+    }
+
+    mapping(uint256 => BoxExchange) public listBoxExchange;
+
+    // map list boxes base on status
+    mapping(Status => BoxItem[]) public listBoxStatus;
 
     constructor(address _BoxNFT) {
         BoxNFT = _BoxNFT;
@@ -55,25 +66,45 @@ contract ExchangeNFT is Ownable {
     function mintToExchange(
         uint256 _id,
         string memory _name,
-        uint8 _status,
         uint8 _rarity,
         uint256 _rarityNumber,
         uint8 _boxType
     ) external onlyOwner {
-        require(_status >= 0 && _status <= 5, "");
         require(_rarity >= 0 && _rarity <= 4, "");
         require(_boxType >= 0 && _boxType <= 1, "");
-        uint256 getAmount = ERC1155Supply(BoxNFT).totalSupply(_id);
-
+        require(_rarityNumber == 1 || _rarityNumber == 10, "");
+        // uint256 getAmount = ERC1155Supply(BoxNFT).totalSupply(_id);
+        uint256 getAmount = 100;
+        console.log(getAmount);
         listBoxExchange[_id] = BoxExchange(
             _id,
             getAmount,
             0,
             _name,
-            Status(_status),
+            Status.NEW,
             Rarity(_rarity),
             _rarityNumber,
             BoxType(_boxType)
         );
+    }
+
+    // add Box Sell to list
+    function addToSaleNormal() external {
+        listBoxStatus[Status(1)].push(
+            BoxItem(
+                1,
+                0xa4adB90b738aA7205e5d79e8CF116F70e1087e3E,
+                10000000000000,
+                Status.SALE_NORMAL
+            )
+        );
+    }
+
+    function getListSellNormal(uint256 _status)
+        external
+        view
+        returns (BoxItem[] memory)
+    {
+        return listBoxStatus[Status(_status)];
     }
 }
