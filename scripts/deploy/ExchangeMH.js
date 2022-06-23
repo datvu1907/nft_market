1387615973b2b5ac806cf7829ab47a24ffac0056
sanvq <sanvq@bap.jp>
2022-06-23 10:59:28 +0700

    add box's local hardhat address

--- scripts/deploy/ExchangeMH.js
+++ scripts/deploy/ExchangeMH.js
@@ -1,15 +1,15 @@
 async function main() {
-    // const boxAddress = "0x042aF2636F718EB89b810Af75aD30c4139883d4F";
     const boxAddress = "0x042aF2636F718EB89b810Af75aD30c4139883d4F";
+    // const boxAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // this address is used for testing on local hardhat network
 
     const [deployer] = await ethers.getSigners();
   
-    console.log("Deploying contracts with the account:", deployer.address);  
+    console.log("Deploying contracts with the account:", deployer.address);
   
     const Exchange = await ethers.getContractFactory("ExchangeMH");
     const exchange = await Exchange.deploy(boxAddress);
   
-    console.log("Exhchange address:", exchange.address);
+    console.log("Exchange address:", exchange.address);
 
     // approve exchange
     const box = await ethers.getContractAt("BoxERC1155", boxAddress);
